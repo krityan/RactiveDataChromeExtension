faa52e908676e2ef51e055bcab2cfe5c28a96daf
krityan <fatalen11@gmail.com>
2017-06-26 09:30:09 +0100

    Rewrote code to be more readable, added component name and computed properties to displayed data.

--- devtools.js
+++ devtools.js
@@ -2,26 +2,68 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+/* Sidebar extension to display details about Ractive Components
+ * Currently displays:
+ * Component name
+ * Data properties
+ * Computed properties
+ */
 chrome.devtools.panels.elements.createSidebarPane(
-  "Ractive Data",
-  function(sidebar) {
-    function update() {
-        var query = 
-            "(!$0 || !$0._ractive) ?" + //if no selected node or selected node is not ractive
-                "{ info: 'Pick a Ractive managed node to see the context.' }" +
-            ":" + //else
-                "$0._ractive.root ?" +  //if ractive root is available (version 0.7)
-                    "$0._ractive.root.get($0._ractive.keypath.str)" + 
-                ":" + //else
-                    "$0._ractive.fragment && $0._ractive.fragment.findContext ?" + //if version 0.8
-                        "$0._ractive.fragment.findContext().get()" +
-                    ":" + //else
-                        "{ info: \'It looks like this is an incompatible Ractive version.\' }"
-        
-      sidebar.setExpression(query);
-    }
+    "Ractive Data Test",
+    function(sidebar) {
 
-    update();
-    chrome.devtools.panels.elements.onSelectionChanged.addListener(update);
-  }
+        /*
+         *  Queries the name of the selected component and passes the result to updatePane
+         */
+        function update() {
+            chrome.devtools.inspectedWindow.eval('Ractive.getNodeInfo($0).ractive.component.name', updatePane);
+        }
+
+        /*
+         *  Parses the getQuery function to a string then wraps it to make it evaluate as an expression. 
+         *  Sends the result to the sidebar
+         *  @param compName the name of the component being queried
+         */
+        function updatePane(compName) {
+            var query = "(" + getQuery.toString() + ")()";
+            sidebar.setExpression(query, compName);
+        }
+        
+        /*
+         * The chrome console query, can use all libraries available to the console as it is not called outside that context.
+         */
+        function getQuery() {
+
+            var properties = {};
+
+            // Data properties
+            if (!$0 || !$0._ractive) { // if there is no (ractive) node selected
+                return {message: 'Select a Ractive node for more details'};
+            } else if ($0._ractive.root) { // Ractive version 0.7
+                Object.assign(properties, Ractive.getNodeInfo($0).ractive.get());       //{{potential better query?}}
+                //Object.assign(properties, $0._ractive.root.get($0._ractive.keypath.str));
+            } else if ($0._ractive.fragment &&$0._ractive.fragment.findContext) { // Ractive version 0.8
+                Object.assign(properties, $0._ractive.fragment.findContext.get());
+            } else {
+                return {message: 'Unsupported Ractive version, 0.7 and 0.8 are currently supported'};
+            }
+
+            // computed properties
+            var comp = Ractive.getNodeInfo($0).ractive.viewmodel.computations;
+            
+            // adds computed properties to data properties and returns the result
+            return Object.keys(comp)
+                .filter(key => !key.startsWith('${'))
+                .reduce((acc, key) => {
+                    acc[key] = comp[key].getter();
+                    return acc;
+                }, properties);
+            
+        }
+
+        // runs initial update
+        update();
+        // every time the selection changes in the elements panel, update is called
+        chrome.devtools.panels.elements.onSelectionChanged.addListener(update); 
+    }
 );
